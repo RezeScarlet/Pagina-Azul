af99894820f53f48e441976eb59854ddf0741fc5
DarkEyeBr <gui171204@gmail.com>
2022-10-24 22:14:14 -0300

    .

--- localhost.sql
+++ localhost.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Banco de Dados: `pagina_azul`
 --
-DROP DATABASE `pagina_azul`;
+DROP DATABASE if EXISTS `pagina_azul`;
 CREATE DATABASE `pagina_azul` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `pagina_azul`;
 
@@ -65,8 +65,7 @@ CREATE TABLE IF NOT EXISTS `anunciante` (
   `telefone` varchar(15) DEFAULT NULL,
   `celular` varchar(15) DEFAULT NULL,
   `whatsapp` varchar(15) DEFAULT NULL,
-  `estado` char(2) DEFAULT NULL,
-  `cidade` varchar(20) DEFAULT NULL,
+  `idCidade` int(11) DEFAULT NULL,
   `CEP` char(9) DEFAULT NULL,
   `bairro` varchar(20) DEFAULT NULL,
   `rua` varchar(30) DEFAULT NULL,
@@ -83,22 +82,22 @@ CREATE TABLE IF NOT EXISTS `anunciante` (
 -- Extraindo dados da tabela `anunciante`
 --
 
-INSERT INTO `anunciante` (`idAnunciante`, `nome`, `CNPJ`, `email`, `website`, `descricao`, `idPlano`, `idCategoria`, `facebook`, `instagram`, `telefone`, `celular`, `whatsapp`, `estado`, `cidade`, `CEP`, `bairro`, `rua`, `numero`, `imgAnuncioP`, `imgAnuncioG`) VALUES
-(1, 'DJ BOOMKITTY', '01.778.130/1000-48', 'djboomkitty@gmail.com', NULL, 'DJ BOOMKITTY, o maior e melhor DJ da região. Agendamos festas e eventos. Com mais de 15 anos de trabalho, somos especializados em som e iluminação. Contate-nos para um momento inesquecível. § festa dj música evento ', 3, 1, 'facebook.com/DJBOOMKITTY', 'intagram.com/DJBOOMKITTY', '92298435', '19992298435', '19992298435', 'SP', 'Mococa', '6789-635', 'Descanço', 'Nicolau Paione', '403', 'djboomkittyP.png', 'djboomkitty.png'),
-(2, 'Fire Design', '02.778.130/2000-48', 'firedesign@contato.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 1, 1, 'facebook.com/firedesign', 'intagram.com/firedesign', '92298435', '19992298435', '19992298435', 'SP', 'Tapiratiba', '6789-635', 'Descanço', 'Nicolau Paione', '403', 'firedesignP.png', 'firedesign.png'),
-(3, 'Ghost Dash', '03.778.130/3000-48', 'ghostdash@gmail.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 2, 1, 'facebook.com/ghostdash', 'intagram.com/ghostdash', '92298435', '19992298435', '19992298435', 'SP', 'Arceburgo', '6789-635', 'Descanço', 'Nicolau Paione', '403', 'ghostdashP.png', 'ghostdash.png'),
-(4, 'Hill Top Co-op', '04.778.130/0001-48', 'hilltopcoop@contato.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 2, 1, 'facebook.com/hilltopcoop', 'intagram.com/hilltopcoop', '92298435', '19992298435', '19992298435', 'SP', 'Mococa', '6789-635', 'Descanço', 'Nicolau Paione', '403', 'hilltopcoopP.png', 'hilltopcoop.png'),
-(5, 'Museum', '05.778.130/0001-48', 'museum@outlook.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 1, 2, 'facebook.com/museum', 'intagram.com/museum', '92298435', '19992298435', '19992298435', 'SP', 'Mococa', '6789-635', 'Descanço', 'Nicolau Paione', '403', 'museumP.png', 'museum.png'),
-(6, 'Orland Groves', '06.778.130/0001-48', 'orlandgroves@gmail.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 2, 12, 'facebook.com/orlandgroves', 'intagram.com/orlandgroves', '92298435', '19992298435', '19992298435', 'SP', 'Mococa', '6789-635', 'Descanço', 'Nicolau Paione', '403', 'orlandgrovesP.png', 'orlandgroves.png'),
-(7, 'Ryan Keller Pharmacy', '07.778.130/0001-48', 'ryankellerpharmacy@sac.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 2, 3, 'facebook.com/ryankellerpharmacy', 'intagram.com/ryankellerpharmacy', '92298435', '19992298435', '19992298435', 'SP', 'Mococa', '6789-635', 'Descanço', 'Nicolau Paione', '403', 'ryankellerpharmacyP.png', 'ryankellerpharmacy.png'),
-(8, 'The Atomic Club', '08.778.130/0001-48', 'atomicclub@hotmail.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 2, 5, 'facebook.com/theatomicclub', 'intagram.com/theatomicclub', '92298435', '19992298435', '19992298435', 'SP', 'Mococa', '6789-635', 'Descanço', 'Nicolau Paione', '403', 'theatomicclubP.png', 'theatomicclub.png'),
-(9, 'Uncle Louies Pizzeria', '09.778.130/0001-48', 'unclelouiespizza@gmail.com', NULL, 'Nós da Pizzaria Uncle Louies fazemos as melhores pizzas de Mococa e região. As pizzas são feitas a mão pelos nosso melhores chefes e assadas no forno a lenha. Está com fome? Então venha nos conhecer. § pizza pizzaria restaurante comida alimentação alimento  ', 3, 1, 'facebook.com/unclelouiespizzeria', 'intagram.com/unclelouiespizzeria', '92298435', '19992298435', '19992298435', 'SP', 'Mococa', '6789-635', 'Descanço', 'Nicolau Paione', '403', 'unclelouiespizzeriaP.png', 'unclelouiespizzeria.png'),
-(10, 'Yurt', '10.778.130/0001-48', 'sac@yurt.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 3, 1, 'facebook.com/yurt', 'intagram.com/yurt', '92298435', '19992298435', '19992298435', 'SP', 'Mococa', '6789-635', 'Descanço', 'Nicolau Paione', '403', 'yurtP.png', 'yurt.png'),
-(11, 'Ponto Barato', NULL, 'arthurrafaeltcc00@gmail.com', 'rafael.projetosfreelancer.com.br', 'O Ponto Barato é um E-Commerce com os preços mais acessíveis possíveis do mercado. Fazemos isso Trabalhando Com Fornecedores Nacionais de Extrema Confiança que trabalham com os melhores materiais usados hoje em dia em Peças de Vestuário, Venha Conhecer nosso Catálogo no Botão abaixo. § moda calçado camisetas camisas ténis jaqueta roupa estilo look ', 3, 7, NULL, NULL, '', '35997733094', '35997733094', NULL, NULL, NULL, NULL, NULL, NULL, 'pontobaratoP.png', 'pontobarato.png'),
-(12, 'Amigos Caridosos', NULL, 'amigoscaridososs@gmail.com', 'amigoscaridosos.projetosetim.com.br', 'A Amigos Caridosos é uma plataforma de doações online, cujo proposito é ajudar pessoas carentes e que necessitam de ajuda por meio de parcerias com instituições filantrópicas, arrecadando recursos para doar de forma prática e eficiente. § ONG caridade doação doar org instituição doações instituições', 3, 13, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'amigoscaridososP.png', 'amigoscaridosos.png'),
-(13, 'Medic On', NULL, 'a@a', NULL, ' § medicina medico saúde terapia ', 3, 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'mediconP.png', 'medicon.png'),
-(15, 'Rota 66', NULL, 'a@a', NULL, ' § roupa moda camiseta calça loja manto esportiva acessórios bermudas shorts futebol ', 3, 7, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'rota66P.png', 'rota66.png'),
-(17, 'Vetclin PetShop', NULL, 'etecgrupo9tcc@gmail.com', NULL, ' § pet veterinário animal ração cachorro gato petshop pet shop', 3, 8, NULL, NULL, NULL, '19981335387', '19981335387', NULL, NULL, NULL, NULL, NULL, NULL, 'vetclinP.png', 'vetclin.png');
+INSERT INTO `anunciante` (`idAnunciante`, `nome`, `CNPJ`, `email`, `website`, `descricao`, `idPlano`, `idCategoria`, `facebook`, `instagram`, `telefone`, `celular`, `whatsapp`, `idCidade`, `CEP`, `bairro`, `rua`, `numero`, `imgAnuncioP`, `imgAnuncioG`) VALUES
+(1, 'DJ BOOMKITTY', '01.778.130/1000-48', 'djboomkitty@gmail.com', NULL, 'DJ BOOMKITTY, o maior e melhor DJ da região. Agendamos festas e eventos. Com mais de 15 anos de trabalho, somos especializados em som e iluminação. Contate-nos para um momento inesquecível. § festa dj música evento ', 3, 1, 'facebook.com/DJBOOMKITTY', 'instagram.com/DJBOOMKITTY', '92298435', '19992298435', '19992298435', 1, '6789-635', 'Descanso', 'Nicolau Paione', '403', 'djboomkittyP.png', 'djboomkitty.png'),
+(2, 'Fire Design', '02.778.130/2000-48', 'firedesign@contato.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 1, 1, 'facebook.com/firedesign', 'instagram.com/firedesign', '92298435', '19992298435', '19992298435', 2, '6789-635', 'Descanso', 'Nicolau Paione', '403', 'firedesignP.png', 'firedesign.png'),
+(3, 'Ghost Dash', '03.778.130/3000-48', 'ghostdash@gmail.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 2, 1, 'facebook.com/ghostdash', 'instagram.com/ghostdash', '92298435', '19992298435', '19992298435', 3, '6789-635', 'Descanso', 'Nicolau Paione', '403', 'ghostdashP.png', 'ghostdash.png'),
+(4, 'Hill Top Co-op', '04.778.130/0001-48', 'hilltopcoop@contato.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 2, 1, 'facebook.com/hilltopcoop', 'instagram.com/hilltopcoop', '92298435', '19992298435', '19992298435', 1, '6789-635', 'Descanso', 'Nicolau Paione', '403', 'hilltopcoopP.png', 'hilltopcoop.png'),
+(5, 'Museum', '05.778.130/0001-48', 'museum@outlook.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 1, 2, 'facebook.com/museum', 'instagram.com/museum', '92298435', '19992298435', '19992298435', 1, '6789-635', 'Descanso', 'Nicolau Paione', '403', 'museumP.png', 'museum.png'),
+(6, 'Orland Groves', '06.778.130/0001-48', 'orlandgroves@gmail.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 2, 2, 'facebook.com/orlandgroves', 'instagram.com/orlandgroves', '92298435', '19992298435', '19992298435', 1, '6789-635', 'Descanso', 'Nicolau Paione', '403', 'orlandgrovesP.png', 'orlandgroves.png'),
+(7, 'Ryan Keller Pharmacy', '07.778.130/0001-48', 'ryankellerpharmacy@sac.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 2, 3, 'facebook.com/ryankellerpharmacy', 'instagram.com/ryankellerpharmacy', '92298435', '19992298435', '19992298435', 1, '6789-635', 'Descanso', 'Nicolau Paione', '403', 'ryankellerpharmacyP.png', 'ryankellerpharmacy.png'),
+(8, 'The Atomic Club', '08.778.130/0001-48', 'atomicclub@hotmail.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 2, 5, 'facebook.com/theatomicclub', 'instagram.com/theatomicclub', '92298435', '19992298435', '19992298435', 1, '6789-635', 'Descanso', 'Nicolau Paione', '403', 'theatomicclubP.png', 'theatomicclub.png'),
+(9, 'Uncle Louies Pizzeria', '09.778.130/0001-48', 'unclelouiespizza@gmail.com', NULL, 'Nós da Pizzaria Uncle Louies fazemos as melhores pizzas de Mococa e região. As pizzas são feitas a mão pelos nosso melhores chefes e assadas no forno a lenha. Está com fome? Então venha nos conhecer. § pizza pizzaria restaurante comida alimentação alimento ', 3, 1, 'facebook.com/unclelouiespizzeria', 'instagram.com/unclelouiespizzeria', '92298435', '19992298435', '19992298435', 1, '6789-635', 'Descanso', 'Nicolau Paione', '403', 'unclelouiespizzeriaP.png', 'unclelouiespizzeria.png'),
+(10, 'Yurt', '10.778.130/0001-48', 'sac@yurt.com', NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 3, 1, 'facebook.com/yurt', 'instagram.com/yurt', '92298435', '19992298435', '19992298435', 1, '6789-635', 'Descanso', 'Nicolau Paione', '403', 'yurtP.png', 'yurt.png'),
+(11, 'Ponto Barato', NULL, 'arthurrafaeltcc00@gmail.com', 'http://rafael.projetosfreelancer.com.br', 'O Ponto Barato é um E-Commerce com os preços mais acessíveis possíveis do mercado. Fazemos isso Trabalhando Com Fornecedores Nacionais de Extrema Confiança que trabalham com os melhores materiais usados hoje em dia em Peças de Vestuário, Venha Conhecer nosso Catálogo no Botão abaixo. § moda calçado camisetas camisas ténis jaqueta roupa estilo look ', 3, 7, NULL, NULL, null, '35997733094', '35997733094', NULL, NULL, NULL, NULL, NULL, 'pontobaratoP.png', 'pontobarato.png'),
+(12, 'Amigos Caridosos', NULL, 'amigoscaridososs@gmail.com', 'http://amigoscaridosos.projetosetim.com.br', 'A Amigos Caridosos é uma plataforma de doações online, cujo proposito é ajudar pessoas carentes e que necessitam de ajuda por meio de parcerias com instituições filantrópicas, arrecadando recursos para doar de forma prática e eficiente. § ONG caridade doação doar org instituição doações instituições', 3, 13, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'amigoscaridososP.png', 'amigoscaridosos.png'),
+(13, 'Medic On', NULL, 'a@a', NULL, ' § medicina medico saúde terapia ', 3, 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'mediconP.png', 'medicon.png'),
+(15, 'Rota 66', NULL, 'a@a', 'http://rota66.projetosetim.com.br', ' § roupa moda camiseta calça loja manto esportiva acessórios bermudas shorts futebol ', 3, 7, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'rota66P.png', 'rota66.png'),
+(17, 'Vetclin PetShop', NULL, 'etecgrupo9tcc@gmail.com', NULL, ' § pet veterinário animal ração cachorro gato petshop pet shop', 3, 8, NULL, NULL, NULL, '19981335387', '19981335387', NULL, NULL, NULL, NULL, NULL, 'vetclinP.png', 'vetclin.png');
 
 -- --------------------------------------------------------
 
@@ -159,11 +158,23 @@ INSERT INTO `planos` (`idPlano`, `nome`, `descricao`) VALUES
 -- Restrições para as tabelas dumpadas
 --
 
+CREATE TABLE `cidades` (
+  `idCidade` int(11) not null AUTO_INCREMENT,
+  `nome` varchar(60) not null,
+  `estado` char(2) not null,
+  PRIMARY KEY (`idCidade`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+INSERT INTO `cidades`(`idCidade`, `nome`, `estado`) VALUES
+(1, 'Mococa', 'SP'),
+(2, 'Tapiratiba', 'SP'),
+(3, 'Arceburgo', 'MG');
 --
 -- Restrições para a tabela `anunciante`
 --
 ALTER TABLE `anunciante`
   ADD CONSTRAINT `idCategoria` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`idCategoria`),
+  ADD CONSTRAINT `idCidade` FOREIGN KEY (`idCidade`) REFERENCES `cidades` (`idCidade`),
   ADD CONSTRAINT `idPlano` FOREIGN KEY (`idPlano`) REFERENCES `planos` (`idPlano`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
