dc8b2304ec8e21a7b87cc5ca56bc12a7e8b1fbaf
Thiago <thiagofkm@gmail.com>
2023-02-25 14:52:18 -0300

    htmlspecialchars() colocado no title da busca

--- pages/busca.php
+++ pages/busca.php
@@ -16,7 +16,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Busca <?php if (isValidSearch()) echo "por " . $_GET["q"] ?> | Página Azul</title>
+  <title>Busca <?php if (isValidSearch()) echo "por " . htmlspecialchars($_GET["q"]) ?> | Página Azul</title>
   <!-- FAVICON -->
   <link rel="shortcut icon" href="/assets/img/logos/favicon.ico" type="image/x-icon">
   <!-- FONT AWESOME -->
